c53bdd43bc2bf58285587d7e789ec2c32cf7a916
Lugi0k <juliodz45@gmail.com>
2023-01-03 17:04:57 -0300

    Se actualiza el Flask

--- 19 El Micro Framework Flask/Flask Test/flasktest.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/D/Documentos/Curso 2/Curso-2/19 El Micro Framework Flask/Flask Test")
-def hello_world():
-    return "<p>Hello, World!</p>"
---
+++ 19 El Micro Framework Flask/Holamundo.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hola mundo'
+
+@app.route('/lala')
+def lala():
+    return 'lala'
+
+@app.route('/lele')
+def lele():
+    return 'lele'
