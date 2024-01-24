0eb64ac4e47209d427bdd9233e6e43df633b63c9
Lugi0ks <juliodz45@gmial.com>
2024-01-24 19:14:27 -0300

    Clase de Respondiendo con JSON

---
+++ 19 El Micro Framework Flask/intro-flask/10 Respondiendo con JSON.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, url_for, redirect, abort, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hola Mundo'
+
+@app.route('/pues/<post_id>' , methods=['GET','POST'])
+def pues(post_id):
+    if request.method == 'GET':
+        return 'el id del post por el metodo get es: ' + post_id
+    else:
+        return 'Estas usando otro metodo distinto al get'
+    
+@app.route('/lele', methods=['POST','GET'])
+def lele():
+    return {
+        "username":'chanquito feliz',
+        "Email":'chanchito@feliz.com'
+    }
+
+# if __name__== '__main__':
+#     app.run(debug=True,
+#             port=5000)
+    
